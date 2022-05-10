f8edb4f953bb147c7e8463a6567e82446fe50df6
Crystal <crystal120599@gmail.com>
2022-05-10 12:37:39 -0700

    start testing add

---
+++ react-app/src/components/ItemListing/ItemListing.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { useDispatch, useSelector } from "react-redux";
+
+function ItemListing(){
+
+}
+
+export default ItemListing;
--- react-app/src/store/item.js
+++ react-app/src/store/item.js
@@ -1,5 +1,6 @@
 const GET_ITEMS = 'item/GET_ITEMS';
 const GET_SINGLE_ITEM = 'item/GET_SINGLE_ITEM';
+const ADD_ITEM = 'item/ADD_ITEM';
 
 const getItems = (items) =>({
     type: GET_ITEMS,
@@ -8,6 +9,11 @@ const getItems = (items) =>({
 
 const getOneItem = (item) => ({
     type: GET_SINGLE_ITEM,
+    payload: item
+})
+
+const addOneItem = (item) =>({
+    type: ADD_ITEM,
     payload: item
 })
 
@@ -32,6 +38,21 @@ export const getOneItemThunk = (itemId) => async dispatch =>{
     return response;
 }
 
+export const addOneItemThunk = (item) => async dispatch =>{
+    const response = await fetch('/api/items/add_item',{
+        method: 'POST',
+        body: item
+    })
+    if (response.ok) {
+        const newItem = await response.json();
+        dispatch(addOneItem(newItem));
+      }else if (response.status < 500) {
+        const data = await response.json();
+        return data
+      }
+      return response;
+}
+
 const initialState = {};
 
 const itemsReducer = (state = initialState, action) =>{
@@ -44,6 +65,9 @@ const itemsReducer = (state = initialState, action) =>{
         case GET_SINGLE_ITEM:
             newState = {...state}
             newState[action.payload.id] = action.payload;
+            return newState;
+        case ADD_ITEM:
+            newState = {[action.payload.id]: action.payload,...state}
             return newState;
         default:
             return state;
