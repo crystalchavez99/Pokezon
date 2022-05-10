0bd87f3d5609530e1f86e5c0fc07cb88e2f621ef
Crystal <crystal120599@gmail.com>
2022-05-10 09:41:23 -0700

    implemented get item redux state

--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -1,9 +1,11 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import session from './session'
+import session from './session';
+import item from './item'
 
 const rootReducer = combineReducers({
   session,
+  item
 });
 
 
--- react-app/src/store/item.js
+++ react-app/src/store/item.js
@@ -10,3 +10,44 @@ const getOneItem = (item) => ({
     type: GET_SINGLE_ITEM,
     payload: item
 })
+
+
+export const getAllItemsThunk = () => async dispatch =>{
+    const response = await fetch('/api/items/')
+    if(response.ok){
+        const items = await response.json()
+        dispatch(getItems(items.items))
+        return items
+    }
+    return response;
+}
+
+export const getOneItemThunk = (itemId) => async dispatch =>{
+    const response = await fetch(`/api/items/${itemId}`)
+    if(response.ok){
+        const item = await response.json();
+        dispatch(getOneItem(item.item))
+        return item
+    }
+    return response;
+}
+
+const initialState = {};
+
+const itemsReducer = (state = initialState, action) =>{
+    let newState;
+    switch(action.type){
+        case GET_ITEMS:
+            newState = {...state}
+            action.payload.forEach(item => newState[item.id] = item)
+            return {...newState,...state}
+        case GET_SINGLE_ITEM:
+            newState = {...state}
+            newState[action.payload.id] = action.payload;
+            return newState;
+        default:
+            return state;
+    }
+}
+
+export default itemsReducer;
