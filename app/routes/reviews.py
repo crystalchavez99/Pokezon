de2bb6614f4b432c8d889835abaa9781c15cfff6
Crystal <crystal120599@gmail.com>
2022-05-10 08:40:35 -0700

    Setting up reviews route

---
+++ app/routes/reviews.py
@@ -0,0 +1,7 @@
+import datetime
+from flask import Blueprint, request
+from flask_login import current_user
+from app.models import Review,db
+from app.api.auth_routes import validation_errors_to_error_messages
+
+review_routes = Blueprint('reviews',__name__,url_prefix="/reviews")
