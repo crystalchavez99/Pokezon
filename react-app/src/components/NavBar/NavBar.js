d93c1f15419729ef2ea90ebb6257a1190ada5c7d
Crystal <crystal120599@gmail.com>
2022-05-10 16:56:14 -0700

    stylized navbar

--- react-app/src/components/NavBar/NavBar.js
+++ react-app/src/components/NavBar/NavBar.js
@@ -10,33 +10,33 @@ const NavBar = () => {
   return (
     <nav id="bot-nav">
       <ul>
-        <li>
+        {/* <li>
           <NavLink to='/' exact={true} activeClassName='active'>
-            <img src={logo} alt='logo'/>
+            <img src={logo} alt='logo' />
           </NavLink>
-        </li>
+        </li> */}
         <form action="/action_page.php">
-          <input type="text" placeholder="Search.." name="search"/>
+          <input type="text" placeholder="Search.." name="search" />
           <button type="submit">Submit</button>
         </form>
-        <li>
-          <NavLink to='/login' exact={true} activeClassName='active'>
-            Login
-          </NavLink>
+        <li id="login-ui">
+            <NavLink to='/login' exact={true} activeClassName='active'>
+            <i class="fa-solid fa-right-to-bracket">Login</i>
+            </NavLink>
         </li>
-        <li>
+        <li id="signup-ui">
           <NavLink to='/sign-up' exact={true} activeClassName='active'>
-            Sign Up
+          <i class="fa-solid fa-user-plus">Sign Up</i>
           </NavLink>
         </li>
         <li>
           <NavLink to={`/users/${user?.id}`} exact={true} activeClassName='active'>
-            Profile
+          <i class="fa-solid fa-user">Profile</i>
           </NavLink>
         </li>
         <li>
-        <NavLink to='/cart' exact={true} activeClassName='active'>
-            Cart
+          <NavLink to='/cart' exact={true} activeClassName='active'>
+          <i class="fa-solid fa-cart-shopping">Cart</i>
           </NavLink>
         </li>
         <li>
