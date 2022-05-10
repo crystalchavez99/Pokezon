a0001f047bde791059a0a3f0c38a65ea17de8ddf
Crystal <crystal120599@gmail.com>
2022-05-10 08:44:37 -0700

    get all reviews work

--- app/routes/reviews.py
+++ app/routes/reviews.py
@@ -5,3 +5,8 @@ from app.models import Review,db
 from app.api.auth_routes import validation_errors_to_error_messages
 
 review_routes = Blueprint('reviews',__name__,url_prefix="/reviews")
+
+@review_routes.route('/')
+def reviews():
+    reviews = Review.query.all()
+    return {'reviews' : [review.to_dict() for review in reviews]}
