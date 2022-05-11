42cafa24a12fabfb4327e3b4fc813251292c811e
Crystal <crystal120599@gmail.com>
2022-05-11 10:09:14 -0700

    item detail start

--- react-app/src/components/ItemDetail/ItemDetail.js
+++ react-app/src/components/ItemDetail/ItemDetail.js
@@ -2,30 +2,34 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { getOneItemThunk } from '../../store/item';
-
+import './ItemDetail.css';
 function ItemDetail() {
     const { itemId } = useParams();
     const dispatch = useDispatch();
     const item = useSelector(state => state?.items[itemId])
     useEffect(() => {
         dispatch(getOneItemThunk(itemId))
-    }, [dispatch,itemId])
+    }, [dispatch, itemId])
 
     return (
-        <div>
-            <h1>Items</h1>
-            <>
-                <img src={item?.image_url} alt={item?.name}/>
-                <p>{item?.name}</p>
-            </>
-            <>
-            <p>₽{item?.price}</p>
-            <p>Quantity: {item?.quantity}</p>
-            </>
+        <div id='item-detail-page'>
+            <div>
+                <p>Home / Balls / {item?.name}</p>
+            </div>
+            <div className='item-display-flex'>
+                <div className='item-image'>
+                    <img src={item?.image_url} alt={item?.name} />
+                </div>
+                <div>
+                    <p>{item?.name}</p>
+                    <p>₽{item?.price}</p>
+                    <p>Quantity: {item?.quantity}</p>
+                    <button>Add To Cart</button>
+                </div>
+            </div>
+
             <>
                 <p>{item?.description}</p>
-
-
             </>
         </div>
     )
