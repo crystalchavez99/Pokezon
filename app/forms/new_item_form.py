121cb0652988e172fddae2ccaef64b3bcda6efe0
Crystal <crystal120599@gmail.com>
2022-05-13 15:23:57 -0700

    require min on price and quantity

--- app/forms/new_item_form.py
+++ app/forms/new_item_form.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField,SubmitField
-from wtforms.validators import DataRequired,Length
+from wtforms.validators import DataRequired,Length,NumberRange
 from app.models import Item
 
 # Form to create a new instance of item
@@ -8,6 +8,6 @@ class NewItem(FlaskForm):
     name = StringField('name',validators=[DataRequired(),Length(max=40)])
     image_url = StringField('image_url',validators=[DataRequired(),Length(max=255)])
     description = StringField('description')
-    price = IntegerField('price',validators=[DataRequired()])
-    quantity = IntegerField('quantity',validators=[DataRequired()])
+    price = IntegerField('price',validators=[DataRequired(),NumberRange(min=1)])
+    quantity = IntegerField('quantity',validators=[DataRequired(),NumberRange(min=1)])
     submit = SubmitField('submit')
