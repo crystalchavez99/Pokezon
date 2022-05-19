be7e27159da9849881a5a8478e0e80536da6928d
Crystal <crystal120599@gmail.com>
2022-05-19 15:10:44 -0700

    When logged out return to home

--- react-app/src/components/auth/LogoutButton.js
+++ react-app/src/components/auth/LogoutButton.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { logout } from '../../store/session';
-
+import { useHistory } from 'react-router-dom';
 const LogoutButton = () => {
   const dispatch = useDispatch()
+  const history = useHistory()
   const onLogout = async (e) => {
     await dispatch(logout());
+    history.push(`/`)
   };
 
   return <a onClick={onLogout}><i className="fa-solid fa-circle-xmark">Logout</i></a>;
