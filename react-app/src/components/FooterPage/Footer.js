df50af9f6d6fc19d019981c9a0abdbc7b4c94dea
Crystal <crystal120599@gmail.com>
2022-06-14 14:27:34 -0700

    footer stays bottom and doesnt interfere with forms

--- react-app/src/components/FooterPage/Footer.js
+++ react-app/src/components/FooterPage/Footer.js
@@ -4,20 +4,14 @@ function Footer() {
     return (
         <footer>
             <div id="reference">
-                <ul>
-                    <li>Crystal Chavez</li>
-                    <li>
-                        <a href="https://github.com/crystalchavez99" target="_blank">
-                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" />
-                        </a>
-                    </li>
-                    <li>
-                        <a href="https://www.linkedin.com/in/cchavez99/" target="_blank">
-                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original-wordmark.svg" />
-                            </a>
+                <p>Crystal Chavez</p>
+                <a href="https://github.com/crystalchavez99" target="_blank">
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" />
+                </a>
+                <a href="https://www.linkedin.com/in/cchavez99/" target="_blank">
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original-wordmark.svg" />
+                </a>
 
-                    </li>
-                </ul>
             </div>
         </footer>
     )
